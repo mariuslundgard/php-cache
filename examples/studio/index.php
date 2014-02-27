3353508332526e276c0039a63f6509792dd0d862
Marius Lundgård <marius.lundgard@gmail.com>
2014-02-27 17:40:04 +0100

    update

--- examples/studio/index.php
+++ examples/studio/index.php
@@ -2,44 +2,66 @@
 
 require __DIR__.'/../../vendor/autoload.php';
 
-try {
-
-    // putenv('DEBUG=1');
-
+function create_app()
+{
     /**
      * A Studio app using Cache\Http\Layer (middleware)
      */
-    (new Studio\Http\Application)
-        ->get('/*', function ($req, $res) {
+    return (new Studio\Http\Application)
+    ->get('/*', function ($req, $res) {
 
-            // retrieve cache
-            if ($req->cache && $cached = $req->cache->fetch('page')) {
-                return $cached;
-            }
+        // retrieve cache
+        if ($req->cache && $cached = $req->cache->fetch('page')) {
+            return $cached;
+        }
 
-            // simulate hard work
-            sleep(1);
+        // simulate hard work
+        usleep(500000); // = 500 ms
 
-            // create view
-            $html = '<h1>Hello, world!</h1>';
+        // create view
+        $html = '<h1>Hello, world!</h1>';
 
-            // store cache value
+        // store cache value
+        if ($req->cache) {
+            // echo '<pre>get from cache</pre>';
             $req->cache->store('page', $html . '<p>Cached: '.gmdate('Y-m-d H:i:s').' GMT</p>');
+        }
 
-            // return view
-            return $html;
+        // return view
+        return $html;
 
-        })
-        ->employ('Cache\Http\Layer', [
-            // 'type' => 'memcached',
-            'type' => 'file',
-            'path' => __DIR__.'/cache',
-        ])
-        ->run();
+    })
+    ->employ('Cache\Http\Layer', [
+        // 'type' => 'memcached',
+        'type' => 'file',
+        'path' => __DIR__.'/cache',
+    ]);
+
 }
 
-catch (Exception $err) {
-    ob_end_clean();
-    echo '<title>'.$err->getMessage().'</title>';
-    echo '<h1>'.$err->getMessage().'</h1>';
+// putenv('DEBUG=1');
+$requests = 0;
+$startTime = float_microtime();
+$duration = 0;
+
+while ($duration < 1) {
+
+    try {
+        $app = create_app();
+        $res = $app->call();
+        $duration = float_microtime() - $startTime;
+        $requests++;
+    }
+
+    catch (Exception $err) {
+        ob_end_clean();
+        echo '<title>'.$err->getMessage().'</title>';
+        echo '<h1>'.$err->getMessage().'</h1>';
+        exit;
+    }
 }
+
+echo $res->body;
+
+echo '<pre>' . number_format( $requests / 1 ) . ' req/sec</pre>';
+// echo '<pre>Ran ' . $requests . ' times in 1 second</pre>'; 
--- src/Cache/Http/Layer.php
+++ src/Cache/Http/Layer.php
@@ -11,15 +11,11 @@ class Layer extends AbstractConfigurableLayer
 {
     public function call(Request $req, Exception $err = null)
     {
-        if ($err) {
+        if ($err || ! $this->config->get('use', true)) {
             return $this->app->call($req);
         }
 
         $req->cache = new Cache($this->config->get());
-
-        // $res = $this->app->call($req);
-
-        // return $res;
 
         return $this->app->call($req);
     }
